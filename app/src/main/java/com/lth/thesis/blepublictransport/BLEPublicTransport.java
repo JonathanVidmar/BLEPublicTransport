799a0d73b6f1463604da44114c302d4b97e661dd
Jonathan Vidmar <dat11jvi@student.lu.se>
2016-02-12 14:55:37 +0100

    moved beaconlogic to application

--- app/src/main/java/com/lth/thesis/blepublictransport/BLEPublicTransport.java
+++ app/src/main/java/com/lth/thesis/blepublictransport/BLEPublicTransport.java
@@ -6,86 +6,72 @@ import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.RemoteException;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.*;
 import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 
-/**
- * Created by dyoung on 12/13/13.
- */
-public class BLEPublicTransport extends Application implements BootstrapNotifier {
+import java.util.Collection;
+
+public class BLEPublicTransport extends Application implements BootstrapNotifier, BeaconConsumer {
     private static final String TAG = "BLEPublicTransport";
     private RegionBootstrap regionBootstrap;
     private BackgroundPowerSaver backgroundPowerSaver;
     private boolean haveDetectedBeaconsSinceBoot = false;
     private MainActivity mainActivity = null;
     private NotificationManager notificationManager;
+    private BeaconManager beaconManager;
 
 
     public void onCreate() {
         super.onCreate();
-        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
-
+        beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().clear();
-        beaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
 
-        Log.d(TAG, "setting up background monitoring for beacons and power saving");
-        // wake up the app when a beacon is seen
-        Region region = new Region("backgroundRegion",
-                null, null, null);
+        // Wakes up application
+        Region region = new Region("backgroundRegion", null, null, null);
         regionBootstrap = new RegionBootstrap(this, region);
-
-        // simply constructing this class and holding a reference to it in your custom Application
-        // class will automatically cause the BeaconLibrary to save battery whenever the application
-        // is not visible.  This reduces bluetooth power usage by about 60%
         backgroundPowerSaver = new BackgroundPowerSaver(this);
         notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // If you wish to test beacon detection in the Android Emulator, you can use code like this:
-        // BeaconManager.setBeaconSimulator(new TimedBeaconSimulator() );
-        // ((TimedBeaconSimulator) BeaconManager.getBeaconSimulator()).createTimedSimulatedBeacons();
     }
 
     @Override
     public void didEnterRegion(Region arg0) {
         // In this example, this class sends a notification to the user whenever a Beacon
         // matching a Region (defined above) are first seen.
-        Log.d(TAG, "did enter region.");
+        Log.d(TAG, "did enter region: " + arg0);
         if (!haveDetectedBeaconsSinceBoot) {
             Log.d(TAG, "auto launching MainActivity");
 
             // The very first time since boot that we detect an beacon, we launch the
-            // MainActivity which in turn launches an activity
+            // MainActivity which in turn launches an fragment
             Intent intent = new Intent(this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             this.startActivity(intent);
             haveDetectedBeaconsSinceBoot = true;
+        } else if (mainActivity != null) {
+            // Currently on a fragment
+            // Send data about beacons{
+
         } else {
-            if (mainActivity != null) {
-                // Currently on a fragment
-                // Send data about beacons
-            } else {
-                // If we have already seen beacons before, but a fragment is not in
-                // the foreground, we send a notification to the user on subsequent detections.
-                Log.d(TAG, "Sending notification.");
-                sendNotification();
-            }
+            // If we have already seen beacons before, but a fragment is not in
+            // the foreground, we send a notification to the user on subsequent detections.
+            Log.d(TAG, "Sending notification.");
+            sendNotification();
         }
-
-
     }
+
 
     @Override
     public void didExitRegion(Region region) {
         // removes notification of entering a region if it exists
-        //notificationManager.cancel(1);
+        notificationManager.cancel(1);
         // update mainActivity that region is no more
     }
 
@@ -112,9 +98,34 @@ public class BLEPublicTransport extends Application implements BootstrapNotifier
         notificationManager.notify(1, builder.build());
     }
 
-    public void setMonitoringActivity(MainActivity activity) {
-        this.mainActivity = activity;
+    @Override
+    public void onBeaconServiceConnect() {
+        startMonitoringBeaconsInRegion();
+        startRangingBeaconsInRegion();
     }
 
+    public void startMonitoringBeaconsInRegion() {
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startRangingBeaconsInRegion() {
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Beacon b = beacons.iterator().next();
+                    Log.i("beacon", "Ranged for beacon: " + b.toString());
+                }
+            }
+        });
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+        }
+    }
 }
 
