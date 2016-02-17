e07e771aacfba3d8789f8b5b6082e51a2b58cfa9
Jacob Arvidsson <jacobarvidsson@me.com>
2016-02-17 10:14:17 +0100

    Added spinner to Payment screen.

--- app/src/main/java/com/lth/thesis/blepublictransport/PaymentFragment.java
+++ app/src/main/java/com/lth/thesis/blepublictransport/PaymentFragment.java
@@ -4,12 +4,17 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,16 +29,18 @@ import com.google.android.gms.common.api.Status;
 import com.google.android.gms.nearby.messages.Message;
 import com.google.android.gms.common.api.ResultCallback;
 
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PaymentFragment extends Fragment {
+public class PaymentFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     // Tag for debugging
     private static final String TAG = "Payment";
     public static final String PREFS_NAME = "MyPrefsFile";
-    private TextView chooseDestination;
-
+    private RelativeLayout chooseDestination;
+    private View view;
 
     public PaymentFragment() {
         // Required empty public constructor
@@ -43,20 +50,51 @@ public class PaymentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_payment, container, false);
+        view = inflater.inflate(R.layout.fragment_payment, container, false);
 
         SharedPreferences settings = getActivity().getSharedPreferences(PREFS_NAME, 0);
         boolean dependant = settings.getBoolean("dependantPayment", false); // true == dependent travel
-        chooseDestination = (TextView) view.findViewById(R.id.chooseDestinationLabel);
+        chooseDestination = (RelativeLayout) view.findViewById(R.id.destinationView);
 
         if(dependant){
             chooseDestination.setVisibility(View.VISIBLE);
+            Spinner spinner = (Spinner) view.findViewById(R.id.destination_spinner);
+            // Create an ArrayAdapter using the string array and a default spinner layout
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.destination_array, R.layout.spinner_destination_item);
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            // Apply the adapter to the spinner
+            spinner.setAdapter(adapter);
+            spinner.setOnItemSelectedListener(this);
         } else {
             chooseDestination.setVisibility(View.INVISIBLE);
+            //chooseDestination.setVisibility((dependant) ? View.VISIBLE : View.INVISIBLE);
+
         }
         setRetainInstance(true);
 
         return view;
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        Resources res = getResources();
+        String[] destinations = res.getStringArray(R.array.destination_array);
+        final String destination =  destinations[pos];
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView infoText = (TextView) getActivity().findViewById(R.id.paymentInfoText);
+                infoText.setText("This trip to " + destination + " will cost: ");
+            }
+        });
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 
     @Override
