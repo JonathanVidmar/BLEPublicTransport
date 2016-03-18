e603589a90d69be078958a59e6dc9e9f757aa1d2
Jacob Arvidsson <jacobarvidsson@me.com>
2016-03-18 16:52:12 +0100

    Updated ticket page.

--- app/src/main/java/com/lth/thesis/blepublictransport/Config/SettingConstants.java
+++ app/src/main/java/com/lth/thesis/blepublictransport/Config/SettingConstants.java
@@ -12,7 +12,7 @@ public class SettingConstants {
 
     public static final String DESTINATION_DEPENDENT_PRICE = "destination_dependent_price";
     public static final String VALID_TICKET_DATE = "valid_ticket_date";
-    public static final String VALID_TICKET_DESTINATION = "valid_ticket_destination";
+    public static final String BOUGHT_TICKET_DATE = "bought_ticket_date";
     public static final String PAY_AUTOMATICALLY = "pay_automatically";
     public static final String HAS_SUBSCRIPTION = "has_subscription";
     public static final String KALMAN_SEEK_VALUE = "kalman_filter_mode";
